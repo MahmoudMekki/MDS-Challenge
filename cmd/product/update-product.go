0d8cb72da19db9584c3eed00ff762d55252317c7
MahmoudMekki <mahmoudmekki3@gmail.com>
2022-06-18 23:02:39 +0200

    feat: implement update product

--- cmd/product/update-product.go
+++ cmd/product/update-product.go
@@ -1,7 +1,24 @@
 package product
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/MahmoudMekki/MDS-task/pkg/models"
+	"github.com/MahmoudMekki/MDS-task/pkg/repo/productsDAL"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
 
 func UpdateProduct(ctx *gin.Context) {
-
+	prod := models.Product{
+		SKU:         ctx.GetString("sku"),
+		CountryCode: ctx.GetString("country_code"),
+		Name:        ctx.GetString("name"),
+	}
+	prod, err := productsDAL.UpdateProduct(prod)
+	if err != nil {
+		log.Err(err).Msg(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while getting the prod"})
+		return
+	}
+	ctx.JSON(http.StatusOK, prod)
 }
--- validators/products.go
+++ validators/products.go
@@ -33,10 +33,30 @@ func ValidateCreateProduct() gin.HandlerFunc {
 }
 func ValidateUpdateProduct() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		sku, ok := ctx.Params.Get("sku")
+		if !ok || len(sku) < 3 {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid sku"})
+			return
+		}
+		jsonData, err := ioutil.ReadAll(ctx.Request.Body)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "bad request"})
+			return
+		}
+		data, err := jio.ValidateJSON(&jsonData, jio.Object().Keys(jio.K{
+			"name":         jio.String().Min(3).Optional(),
+			"country_code": jio.String().Optional(),
+		}))
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.Set("sku", sku)
+		ctx.Set("name", data["name"])
+		ctx.Set("country_code", data["country_code"])
+		ctx.Next()
 	}
 }
-
 func ValidateGetProduct() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		sku, ok := ctx.Params.Get("sku")
