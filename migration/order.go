774f12a888d383bf83db5bd47a3628b0e63631ed
MahmoudMekki <mahmoudmekki3@gmail.com>
2022-06-18 21:03:04 +0200

    feat: implementing the migration

--- migration/order.go
+++ migration/order.go
@@ -1,9 +1,21 @@
 package migration
 
-import "github.com/go-gormigrate/gormigrate/v2"
+import (
+	"github.com/MahmoudMekki/MDS-task/pkg/models"
+	"github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
+)
 
 func listOrder() []*gormigrate.Migration {
 	return []*gormigrate.Migration{
-		{},
+		{
+			ID: "create-orders-table-and-id-index",
+			Migrate: func(db *gorm.DB) error {
+				return db.AutoMigrate(&models.Order{})
+			},
+			Rollback: func(db *gorm.DB) error {
+				return db.Migrator().DropTable(models.OrdersTableName)
+			},
+		},
 	}
 }
--- migration/product.go
+++ migration/product.go
@@ -1,11 +1,21 @@
 package migration
 
 import (
+	"github.com/MahmoudMekki/MDS-task/pkg/models"
 	"github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
 )
 
 func listProduct() []*gormigrate.Migration {
 	return []*gormigrate.Migration{
-		{},
+		{
+			ID: "create-products-table-and-id-index",
+			Migrate: func(db *gorm.DB) error {
+				return db.AutoMigrate(&models.Product{})
+			},
+			Rollback: func(db *gorm.DB) error {
+				return db.Migrator().DropTable(models.ProductsTableName)
+			},
+		},
 	}
 }
